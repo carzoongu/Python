6934c9381977720f1b8fd7847eb452d3fcb11612
Benson Kazungu <108935596+carzoongu@users.noreply.github.com>
2024-03-16 02:06:12 +0300

    Update currencyConverter.py

--- currencyConverter.py
+++ currencyConverter.py
@@ -1,7 +1,7 @@
 from forex_python.converter import currencyRates
 c = currencyRates()
 amount = int(input("Enter Amount: "))
-from_currency = input("From currency: ").upper()
+from_currency =  input("From currency: ").upper()
 to_currency = input("To cureency: ").upper
 print(from_currency, " To ", to_currency, amount)
 result = c.convert(from_currency, to_currency, amount)
